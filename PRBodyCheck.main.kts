16b77dd1f0fc9debc094bcea10670389ee826221
theapache64 <theapache64@gmail.com>
2024-09-20 00:14:06 +0530

    🆙 update featuere

--- PRBodyCheck.main.kts
+++ PRBodyCheck.main.kts
@@ -4,7 +4,7 @@ import java.io.File
 
 val isDebug = true
 var prBody: String? = System.getenv("PR_BODY")
-println("QuickTag: :hello: ")
+println("QuickTag: :hello from feature ")
 prBody = if (prBody == null && isDebug) {
     println("Providing dummy PR body...")
     """
