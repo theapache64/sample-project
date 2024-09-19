0b262c6575d7e2392ed6d3e0c8833c86dbdf8285
theapache64 <theapache64@gmail.com>
2024-09-19 23:44:22 +0530

    update script

--- PRBodyCheck.main.kts
+++ PRBodyCheck.main.kts
@@ -2,4 +2,75 @@
 
 import java.io.File
 
-println("Hello from Kotlin script !${System.getenv("PR_BODY")}!")
+val isDebug = true
+var prBody : String? = System.getenv("GITHUB_ACCESS_TOKEN")
+prBody = if (prBody == null && isDebug) {
+    println("Providing dummy PR body...")
+    """
+        <!-- Comment on this document to propose changes: https://my-company.atlassian.net/wiki/spaces/HP2/pages/1588396037/Github+PR+template --> 
+        ## PR Type
+        Please keep the type of change your PR introduces and remove others
+        - FEATURE - Addition of functionality
+        - BUG FIX - Fixes existing functionality
+        - CODE REFACTOR & CLEANUP - Removing unused configurations or code cleaning
+        - PERF - Performance improvement
+        - TESTS - Only contains changes and additions to tests
+        - RELEASE - App version upgrade etc.
+        - OTHER - README update, PR template update, etc
+
+        ## PR Checks
+        Please add a comment `run test` to run maestro and UI tests. Check [here](https://my-company.slack.com/archives/C01SUJ1CDFF/p1684930174247179) for more details
+        - something 
+
+        ## Why more than 500 lines of code?
+        this is my reason
+
+        ## Description
+        <!-- Please explain your changes -->
+
+        ## Documents and other resources
+        <!-- Link to Engineering Doc, Zeplin links, Product Doc and others -->
+
+        ## Other Impacted Features (side effects)
+        <!-- For Example:
+             Continue Watching 
+             Subscription Paywall
+             Billboard Ads -->
+
+        ## Checklist
+        - [ ] My PR only includes one change. There are no additional changes mixed in this single PR (mandatory)
+        - [ ] I have made corresponding changes to the documentation
+        - [ ] I have tested on a release build
+        - [ ] I have tested this change with leak canary enabled for any leaks
+        - [ ] I have got the design reviewed from the design team
+        - [ ] This change is config controlled
+
+        ##  For SDETs Only
+        - [ ] E2E Test cases are covered for the PR
+
+        ## Testing
+        Slack Link to the build in #android-my-company-x-build channel from GoCD pipeline:
+        <!-- For example https://my-company.slack.com/archives/CD7Q1L7J7/p1589185464304800 -->
+
+        <!-- Guide to using pipeline: https://my-company.atlassian.net/wiki/spaces/HP2/pages/1676641409/Android+Pipelines+Overview -->
+        <!-- Note: The link to the build should have the changes enabled by default. -->
+
+        ## Before / After Change Screenshots
+        <!-- For UI/UX changes. Can be Gif / screenshot. -->
+
+        ## Is additional manual testing needed?
+        - [ ] Yes
+        - [ ] No
+    """.trimIndent()
+} else {
+    prBody
+}
+
+if (prBody.isNullOrEmpty()) {
+    error("prBody is emptyOrNull -> '$prBody'")
+}
+
+val prTemplate = File(".github/PULL_REQUEST_TEMPLATE.md")
+prTemplate.readText().also {
+    println(it)
+}
